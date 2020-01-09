10c5246bb883ea7ee972b7713f6d2bf33fa23f97
Sergey Litvinov <slitvinov@gmail.com>
2020-01-09 21:36:36 +0100

    wip

--- old/run3d.sh
+++ old/run3d.sh
@@ -5,7 +5,7 @@ set -u
 rm -rf dum* im* poly* log.lammps
 lmp -in sdpd-polymer3D-inti.lmp
 
-awk -v cutoff=3.0 -v Nbeads=10 -v Nsolvent=0 -v Npoly=full \
+awk -v cutoff=3.0 -v Nbeads=10 -v Nsolvent=1000 -v Npoly=full \
     -f addpolymer.awk poly3d.txt > poly3.txt
 nbound=$(tail -n 1 poly3.txt | awk '{print $1}')
 sed "s/_NUMBER_OF_BOUNDS_/$nbound/1" poly3.txt > poly3d.txt
